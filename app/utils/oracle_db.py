ee2b7ca3e864dd48eb8ac5bf11cc6ec8493e306d
Untara Vivi Chahya <vyvy1777@gmail.com>
2025-10-15 14:51:28 +0700

    default db thin

--- app/utils/oracle_db.py
+++ app/utils/oracle_db.py
@@ -5,6 +5,8 @@ _pool = None  # global pool instance
 
 def init_pool():
     global _pool
+    oracledb.defaults.thin = True
+    print("is_thin_mode =", oracledb.is_thin_mode())
     if _pool is None:
         dsn = f"//{settings.ORACLE_DB_HOST}:{settings.ORACLE_DB_PORT}/{settings.ORACLE_DB_SERVICE}"  # classic EZConnect syntax
         _pool = oracledb.create_pool(
